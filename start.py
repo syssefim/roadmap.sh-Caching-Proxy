b737384dc7d6b26545267c7386ea053961c50618
syssefim <serafim.sharkov@gmail.com>
2026-08-02 21:37:35 -0700

    Started implementing argparse

--- start.py
+++ start.py
@@ -1,4 +1,27 @@
 from flask import Flask
+import argparse
+
+#argparse
+
+# 1. Create the parser
+parser = argparse.ArgumentParser(description="a simple greet")
+
+# 2. Add argument
+#parser.add_argument("name", help="the name of the person to greet")
+parser.add_argument("flask_port", help="the port to run flask on")
+
+# 3. Parse the arguments
+args = parser.parse_args()
+
+#print(f"Hi, {args.name}")
+
+
+
+
+
+
+
+#flask
 
 app = Flask(__name__)
 
@@ -8,4 +31,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(port=args.flask_port)
