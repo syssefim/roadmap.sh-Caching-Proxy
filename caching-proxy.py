7842c02a7e31d0747136151eb2293d857a208f2a
syssefim <serafim.sharkov@gmail.com>
2026-08-04 17:31:53 -0700

    Added the ability to navigate subpages and also began implementing cache

--- caching-proxy.py
+++ caching-proxy.py
@@ -1,15 +1,19 @@
 from flask import Flask
-import argparse, requests
+import argparse
+import requests
+import json
+import os
+
 
 #argparse
 
-# How to use: python3 caching-proxy.py num
+# How to use: python3 caching-proxy.py --port <number> --origin <url>
+# Example: python3 caching-proxy.py --port 3000 --origin http://dummyjson.com
 
 # 1. Create the parser
 parser = argparse.ArgumentParser(description="caching-proxy")
 
 # 2. Add argument
-#parser.add_argument("name", help="the name of the person to greet")
 parser.add_argument("--port", type=int, default=3000, help="the port to run flask on")
 parser.add_argument("--origin", type=str, help="URL of the server to which the requests will be forwarded")
 
@@ -17,9 +21,10 @@ parser.add_argument("--origin", type=str, help="URL of the server to which the r
 # 3. Parse the arguments
 args = parser.parse_args()
 
-#print(f"Hi, {args.name}")
 
+#cache
 
+CACHE_FILE = 'cache.json'
 
 
 
@@ -31,10 +36,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    response = requests.get(args.origin)
+    with open(CACHE_FILE, 'r') as f:
+        data = json.load(f)
 
-    return response.text
+    if args.origin in data:
+        print('X-Cache: HIT')
+        return "hi"
+    else:
+        print('X-Cache: MISS')
+
+        # Save request to cache
+
+        return url_request.text
+
+
+@app.route("/<path:subpage>")
+def subpage(subpage):
+    sub_url = requests.get(args.origin + "/" + subpage)
+    return sub_url.text
 
 
 if __name__ == "__main__":
+    url_request = requests.get(args.origin)
+
     app.run(port=args.port)
