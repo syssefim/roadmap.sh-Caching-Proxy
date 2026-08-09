aad848dac6a04e67c84d2074a407df25d10472b9
syssefim <serafim.sharkov@gmail.com>
2026-08-09 13:31:51 -0700

    Implemented cache clearing

--- caching-proxy.py
+++ caching-proxy.py
@@ -1,23 +1,49 @@
 from flask import Flask
-import argparse, requests, sqlite3
+import argparse, requests, sqlite3, sys, os
+
+#global variable(s)
+CACHE_DB_NAME = "cache.db"
+
 
 #argparse
 
-# How to use: python3 caching-proxy.py --port <number> --origin <url>
-# Example: python3 caching-proxy.py --port 3000 --origin http://dummyjson.com
-
-
 # 1. Create the parser
-parser = argparse.ArgumentParser(description="caching-proxy")   
+parser = argparse.ArgumentParser(description="caching-proxy")
 
-# 2. Add argument
-#parser.add_argument("name", help="the name of the person to greet")
-parser.add_argument("--port", type=int, default=3000, help="the port to run flask on")
-parser.add_argument("--origin", type=str, default="https://github.com", help="URL of the server to which the requests will be forwarded")
+# 2. Add the arguments
+parser.add_argument(
+    "--port",
+    type=int,
+    default=3000,
+    help="Port on which the proxy server will run"
+)
 
+parser.add_argument(
+    "--origin",
+    type=str,
+    default="https://github.com/syssefim",
+    help="The origin URL to forward requests to"
+)
+
+parser.add_argument(
+    "--clear-cache",
+    action="store_true",
+    help="Clear the cache database"
+)
 
 # 3. Parse the arguments
 args = parser.parse_args()
+
+# 4. Handle the input logic
+if args.clear_cache:
+    print("Clearing proxy db cache...")
+
+    if os.path.exists(CACHE_DB_NAME):
+        os.remove(CACHE_DB_NAME)
+
+    sys.exit()
+
+    
 
 PORT = args.port
 URL = args.origin
@@ -30,7 +56,9 @@ URL = args.origin
 
 
 
-#flask
+
+
+#flask + sqlite
 
 app = Flask(__name__)
 
@@ -38,8 +66,10 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/<path:subpage>")
 def page(subpage=""):
-    # Connect to a database file (or create it)
-    conn = sqlite3.connect('cache.db')
+    # Connect to a database file (or create it) u
+    conn = sqlite3.connect(CACHE_DB_NAME)
+
+    conn.row_factory = sqlite3.Row
 
     # Create a cursor object to interact with the database
     cursor = conn.cursor()
@@ -63,7 +93,7 @@ def page(subpage=""):
 
     if row:
         print("X-Cache: HIT")
-        return "X-Cache: HIT"
+        return row["response"]
     else:
         print("X-Cache: MISS")
         sub_url = requests.get(URL + ("" if URL.endswith("/") else "/") + subpage)
@@ -77,4 +107,4 @@ def page(subpage=""):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=PORT)
+    app.run(port=PORT)
